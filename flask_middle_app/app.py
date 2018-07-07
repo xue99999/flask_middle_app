4da131b8ed33bd8b3cb1aa6a105272814314e31b
xue99999 <1090374167@qq.com>
2018-07-07 18:36:59 +0800

    complete

---
+++ flask_middle_app/app.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask_middle_app.config import configs
+from flask_middle_app.models import db, Course
+
+
+def create_app(config):
+    app = Flask(__name__)
+
+    app.config.from_object(configs.get(config))
+
+    db.init_app(app)
+    register_blueprints(app)
+
+    return app
+
+def register_blueprints(app):
+    from .handlers import front, course, admin
+    app.register_blueprint(front)
+    app.register_blueprint(course)
+    app.register_blueprint(admin)
+
+# create tables
+def create_data():
+    db.create_all()
+    user = User(username='admin')
+    course1 = Course(name='python course', author=user)
+    course2 = Course(name='flask course', author=user)
+    db.session.add(user)
+    db.session.add(course1)
+    db.session.add(course2)
+    db.session.commit()
+
+if __name__ == '__main__':
+    create_data()
---
+++ manage.py
@@ -0,0 +1,6 @@
+from flask_middle_app.app import create_app
+
+app = create_app('development')
+
+if __name__ == '__main__':
+    app.run()
